5631dc707fe65b910e0dde90991a7da6114d56d6
Денис <podolachenko@gmail.com>
2018-03-06 15:41:06 +0200

    внедряю nxbootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 
-import { AppComponent } from './app.component';
-import { TopComponent } from './top/top.component';
-import { ProfileCharPipe } from './top/profile-char.pipe';
+import {AppComponent} from './app.component';
+import {TopComponent} from './top/top.component';
+import {ProfileCharPipe} from './top/profile-char.pipe';
+import { NgbDropdownModule, NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TopComponent,
-    ProfileCharPipe
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        TopComponent,
+        ProfileCharPipe
+    ],
+    imports: [
+        BrowserModule,
+        NgbModule.forRoot(),
+        NgbDropdownModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/top/top.component.ts
+++ src/app/top/top.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NgbDropdownConfig} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
-  selector: 'app-top',
-  templateUrl: './top.component.html',
-  styleUrls: ['./top.component.scss']
+    selector: 'app-top',
+    templateUrl: './top.component.html',
+    styleUrls: ['./top.component.scss'],
+    providers: [NgbDropdownConfig]
 })
 export class TopComponent implements OnInit {
 
-  constructor() { }
+    constructor(config: NgbDropdownConfig) {
+        config.placement = 'top-left';
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
